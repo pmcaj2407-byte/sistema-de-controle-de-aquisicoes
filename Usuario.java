eef789cddc921551b840dafc844462581fcb5124
Leonardo Scolmeister <Leonardo.scolmeister@edu.pucrs.br>
2026-09-20 19:57:40 -0300

    Realizando alterações de lógica

--- Usuario.java
+++ Usuario.java
@@ -2,15 +2,23 @@ public abstract class Usuario {
 
     private int id; 
     private String nome; 
-    private Tipo tipo;
+    private TipoUsuario tipo;
+    private Departamento dep;
 
-
-    public Usuario(int id, String nome, Tipo tipo) {
+    public Usuario(int id, String nome, TipoUsuario tipo, Departamento dep) {
         this.id=id;
         this.nome = nome; 
         this.tipo = tipo;
+        this.dep = dep;
 
     }
+    public Departamento getDep() {
+        return dep;
+    }
+    public void setDep(Departamento dep) {
+        this.dep = dep;
+    }
+    
     public int getId() {
         return id;
     }
@@ -26,11 +34,11 @@ public abstract class Usuario {
         this.nome = nome;
     }
 
-    public Tipo getTipo() {
+    public TipoUsuario getTipo() {
         return tipo;
     }
 
-    public void setTipo(Tipo tipo) {
+    public void setTipo(TipoUsuario tipo) {
         this.tipo = tipo;
     }
 }
