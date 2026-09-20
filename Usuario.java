59ce5421164807d399e1a267d0f5ae94faefc33f
Leonardo Scolmeister <Leonardo.scolmeister@edu.pucrs.br>
2026-09-20 20:18:48 -0300

    Realizando correções de lógica.

--- Usuario.java
+++ Usuario.java
@@ -3,14 +3,22 @@ public abstract class Usuario {
     private int id; 
     private String nome; 
     private Tipo tipo;
+    private Departamento dep;
 
-
-    public Usuario(int id, String nome, Tipo tipo) {
+    public Usuario(int id, String nome, Tipo tipo, Departamento dep) {
         this.id=id;
         this.nome = nome; 
         this.tipo = tipo;
+        this.dep=dep;
 
     }
+    public Departamento getDep() {
+        return dep;
+    }
+    public void setDep(Departamento dep) {
+        this.dep = dep;
+    }
+    
     public int getId() {
         return id;
     }
