17ecb8806ae429c385ba161c384a86ca01f30ee8
pedro <pmcaj2407@gmail.com>
2026-09-20 19:43:47 -0300

    fazendo ajustes e adicionando coisas

--- Usuario.java
+++ Usuario.java
@@ -3,12 +3,14 @@ public abstract class Usuario {
     private int id; 
     private String nome; 
     private Tipo tipo;
+    private Departamento departamento;
 
 
-    public Usuario(int id, String nome, Tipo tipo) {
+    public Usuario(int id, String nome, Tipo tipo, Departamento departamento) {
         this.id=id;
         this.nome = nome; 
         this.tipo = tipo;
+        this.departamento = departamento;
 
     }
     public int getId() {
@@ -32,5 +34,24 @@ public abstract class Usuario {
 
     public void setTipo(Tipo tipo) {
         this.tipo = tipo;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(Departamento departamento){
+        this.departamento = departamento;
+    }
+
+    public boolean Administrador(){
+        return tipo == tipo.ADMINISTRADOR;
+    }
+
+    @Override
+    public String toString(){
+        return "Id: " + id +
+                "Nome: " + nome +
+                "tipo: " + tipo +
+                "Departamento: " + departamento.getNome();
     }
 }
